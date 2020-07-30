e216b6301414fa26af425ad4ff89c6402ce9f6f2
James Chance <38363488+jameschancetech@users.noreply.github.com>
2020-07-30 21:29:48 +0100

    Scroll eventlistener added

--- script.js
+++ script.js
@@ -51,5 +51,15 @@ async function getPhotos() {
   }
 }
 
+// Check to see if scrolling near bottom of page, load more photos
+window.addEventListener(`scroll`, () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos();
+  }
+});
+
 //On load
 getPhotos();
