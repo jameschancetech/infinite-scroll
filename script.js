e42456f8c174e2228ad16a954dd2e51325496709
James Chance <38363488+jameschancetech@users.noreply.github.com>
2020-07-31 20:50:46 +0100

    Changed the initial count value to 5

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let totalImages = 0;
 let photosArray = [];
 
 //Unsplash API
-const count = 30;
+const count = 5;
 const apiKey = `19QH48QDMFQycSojya5b4uoEAycIO06OnhICLgj_6xI`;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -18,6 +18,7 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    count = 30;
   }
 }
 
